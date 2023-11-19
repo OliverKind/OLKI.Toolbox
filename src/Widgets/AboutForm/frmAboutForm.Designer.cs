cdc0059f30b471f14b99b8048b5a59e159b20881
Oliver Kind <github@oliver-kind.de>
2023-11-19 10:51:49 +0100

    Fixed controle sizing issues, Add Update controle elemnts (Not working at this point)

--- src/Widgets/AboutForm/frmAboutForm.Designer.cs
+++ src/Widgets/AboutForm/frmAboutForm.Designer.cs
@@ -28,63 +28,21 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutForm));
-            this.lblProductName = new System.Windows.Forms.Label();
-            this.lblVersion = new System.Windows.Forms.Label();
-            this.lblCopyright = new System.Windows.Forms.Label();
-            this.lblCompanyName = new System.Windows.Forms.Label();
             this.pbxLogoApplication = new System.Windows.Forms.PictureBox();
             this.txtDescription = new System.Windows.Forms.TextBox();
             this.txtCredits = new System.Windows.Forms.TextBox();
             this.pbxLogoFile = new System.Windows.Forms.PictureBox();
             this.btnGoToLicenses = new System.Windows.Forms.Button();
             this.btnClose = new System.Windows.Forms.Button();
+            this.chkCheckUpdatesOnStartup = new System.Windows.Forms.CheckBox();
+            this.lblCheckUpdates = new System.Windows.Forms.LinkLabel();
+            this.lblProductName = new System.Windows.Forms.Label();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.lblCopyright = new System.Windows.Forms.Label();
+            this.lblCompanyName = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pbxLogoApplication)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbxLogoFile)).BeginInit();
             this.SuspendLayout();
-            // 
-            // lblProductName
-            // 
-            this.lblProductName.Location = new System.Drawing.Point(146, 12);
-            this.lblProductName.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
-            this.lblProductName.MaximumSize = new System.Drawing.Size(0, 17);
-            this.lblProductName.Name = "lblProductName";
-            this.lblProductName.Size = new System.Drawing.Size(446, 17);
-            this.lblProductName.TabIndex = 0;
-            this.lblProductName.Text = "Progamm: {0}";
-            this.lblProductName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // lblVersion
-            // 
-            this.lblVersion.Location = new System.Drawing.Point(146, 38);
-            this.lblVersion.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
-            this.lblVersion.MaximumSize = new System.Drawing.Size(0, 17);
-            this.lblVersion.Name = "lblVersion";
-            this.lblVersion.Size = new System.Drawing.Size(446, 17);
-            this.lblVersion.TabIndex = 1;
-            this.lblVersion.Text = "Version: {0} ";
-            this.lblVersion.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // lblCopyright
-            // 
-            this.lblCopyright.Location = new System.Drawing.Point(146, 64);
-            this.lblCopyright.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
-            this.lblCopyright.MaximumSize = new System.Drawing.Size(0, 17);
-            this.lblCopyright.Name = "lblCopyright";
-            this.lblCopyright.Size = new System.Drawing.Size(446, 17);
-            this.lblCopyright.TabIndex = 2;
-            this.lblCopyright.Text = "Copyright: {0}";
-            this.lblCopyright.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // lblCompanyName
-            // 
-            this.lblCompanyName.Location = new System.Drawing.Point(146, 90);
-            this.lblCompanyName.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
-            this.lblCompanyName.MaximumSize = new System.Drawing.Size(0, 17);
-            this.lblCompanyName.Name = "lblCompanyName";
-            this.lblCompanyName.Size = new System.Drawing.Size(446, 17);
-            this.lblCompanyName.TabIndex = 3;
-            this.lblCompanyName.Text = "Entwickelt durch: {0}";
-            this.lblCompanyName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // pbxLogoApplication
             // 
@@ -104,7 +62,7 @@
             this.txtDescription.ReadOnly = true;
             this.txtDescription.ScrollBars = System.Windows.Forms.ScrollBars.Both;
             this.txtDescription.Size = new System.Drawing.Size(446, 126);
-            this.txtDescription.TabIndex = 14;
+            this.txtDescription.TabIndex = 6;
             this.txtDescription.TabStop = false;
             this.txtDescription.Text = "Beschreibung";
             this.txtDescription.WordWrap = false;
@@ -118,7 +76,7 @@
             this.txtCredits.ReadOnly = true;
             this.txtCredits.ScrollBars = System.Windows.Forms.ScrollBars.Both;
             this.txtCredits.Size = new System.Drawing.Size(446, 194);
-            this.txtCredits.TabIndex = 15;
+            this.txtCredits.TabIndex = 7;
             this.txtCredits.TabStop = false;
             this.txtCredits.Text = resources.GetString("txtCredits.Text");
             this.txtCredits.WordWrap = false;
@@ -137,7 +95,7 @@
             this.btnGoToLicenses.Location = new System.Drawing.Point(146, 448);
             this.btnGoToLicenses.Name = "btnGoToLicenses";
             this.btnGoToLicenses.Size = new System.Drawing.Size(75, 20);
-            this.btnGoToLicenses.TabIndex = 17;
+            this.btnGoToLicenses.TabIndex = 8;
             this.btnGoToLicenses.Text = "&Lizenzen";
             // 
             // btnClose
@@ -146,24 +104,84 @@
             this.btnClose.Location = new System.Drawing.Point(517, 448);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(75, 20);
-            this.btnClose.TabIndex = 18;
+            this.btnClose.TabIndex = 9;
             this.btnClose.Text = "&OK";
+            // 
+            // chkCheckUpdatesOnStartup
+            // 
+            this.chkCheckUpdatesOnStartup.AutoSize = true;
+            this.chkCheckUpdatesOnStartup.Location = new System.Drawing.Point(485, 38);
+            this.chkCheckUpdatesOnStartup.Name = "chkCheckUpdatesOnStartup";
+            this.chkCheckUpdatesOnStartup.Size = new System.Drawing.Size(107, 17);
+            this.chkCheckUpdatesOnStartup.TabIndex = 3;
+            this.chkCheckUpdatesOnStartup.Text = "Beim Start prüfen";
+            this.chkCheckUpdatesOnStartup.UseVisualStyleBackColor = true;
+            this.chkCheckUpdatesOnStartup.Visible = false;
+            // 
+            // lblCheckUpdates
+            // 
+            this.lblCheckUpdates.AutoSize = true;
+            this.lblCheckUpdates.Location = new System.Drawing.Point(394, 38);
+            this.lblCheckUpdates.Name = "lblCheckUpdates";
+            this.lblCheckUpdates.Size = new System.Drawing.Size(85, 13);
+            this.lblCheckUpdates.TabIndex = 2;
+            this.lblCheckUpdates.TabStop = true;
+            this.lblCheckUpdates.Text = "Updates suchen";
+            this.lblCheckUpdates.Visible = false;
+            // 
+            // lblProductName
+            // 
+            this.lblProductName.Location = new System.Drawing.Point(146, 12);
+            this.lblProductName.Name = "lblProductName";
+            this.lblProductName.Size = new System.Drawing.Size(446, 17);
+            this.lblProductName.TabIndex = 0;
+            this.lblProductName.Text = "Progamm: {0}";
+            this.lblProductName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.Location = new System.Drawing.Point(146, 38);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(242, 17);
+            this.lblVersion.TabIndex = 1;
+            this.lblVersion.Text = "Version: {0} ";
+            this.lblVersion.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblCopyright
+            // 
+            this.lblCopyright.Location = new System.Drawing.Point(146, 64);
+            this.lblCopyright.Name = "lblCopyright";
+            this.lblCopyright.Size = new System.Drawing.Size(446, 17);
+            this.lblCopyright.TabIndex = 4;
+            this.lblCopyright.Text = "Copyright: {0}";
+            this.lblCopyright.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblCompanyName
+            // 
+            this.lblCompanyName.Location = new System.Drawing.Point(146, 90);
+            this.lblCompanyName.Name = "lblCompanyName";
+            this.lblCompanyName.Size = new System.Drawing.Size(446, 17);
+            this.lblCompanyName.TabIndex = 5;
+            this.lblCompanyName.Text = "Entwickelt durch: {0}";
+            this.lblCompanyName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // AboutForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(604, 480);
+            this.Controls.Add(this.lblCompanyName);
+            this.Controls.Add(this.lblCopyright);
+            this.Controls.Add(this.lblVersion);
+            this.Controls.Add(this.lblProductName);
+            this.Controls.Add(this.lblCheckUpdates);
+            this.Controls.Add(this.chkCheckUpdatesOnStartup);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.btnGoToLicenses);
             this.Controls.Add(this.pbxLogoFile);
             this.Controls.Add(this.txtCredits);
             this.Controls.Add(this.txtDescription);
             this.Controls.Add(this.pbxLogoApplication);
-            this.Controls.Add(this.lblProductName);
-            this.Controls.Add(this.lblVersion);
-            this.Controls.Add(this.lblCopyright);
-            this.Controls.Add(this.lblCompanyName);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -181,15 +199,17 @@
         }
 
         #endregion
-        private System.Windows.Forms.Label lblProductName;
-        private System.Windows.Forms.Label lblVersion;
-        private System.Windows.Forms.Label lblCopyright;
-        private System.Windows.Forms.Label lblCompanyName;
         private System.Windows.Forms.PictureBox pbxLogoApplication;
         private System.Windows.Forms.TextBox txtDescription;
         private System.Windows.Forms.TextBox txtCredits;
         private System.Windows.Forms.PictureBox pbxLogoFile;
         private System.Windows.Forms.Button btnGoToLicenses;
         private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.CheckBox chkCheckUpdatesOnStartup;
+        private System.Windows.Forms.LinkLabel lblCheckUpdates;
+        private System.Windows.Forms.Label lblProductName;
+        private System.Windows.Forms.Label lblVersion;
+        private System.Windows.Forms.Label lblCopyright;
+        private System.Windows.Forms.Label lblCompanyName;
     }
 }
