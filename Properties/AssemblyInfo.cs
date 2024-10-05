53b5d3eb5035b087aaae77b719d2ccb8ac2b69fd
Oliver Kind <github@oliver-kind.de>
2024-10-05 10:55:11 +0200

    Update to version 6.0.0.20

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("OLKI-Software")]
 [assembly: AssemblyProduct("OLKI.Toolbox")]
-[assembly: AssemblyCopyright("Copyright ©  2023 - Oliver Kind")]
+[assembly: AssemblyCopyright("Copyright ©  2024 - Oliver Kind")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // indem Sie "*" wie unten gezeigt eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.5.0.19")]
-[assembly: AssemblyFileVersion("5.5.0.19")]
+[assembly: AssemblyVersion("6.0.0.20")]
+[assembly: AssemblyFileVersion("6.0.0.20")]
